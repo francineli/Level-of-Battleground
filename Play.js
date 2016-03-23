2184b35520a476af562ce2e7ed370378ce740cc4
francineli <francineli1108@gmail.com>
2016-03-22 18:14:28 -0700

    updated11
    updated11

--- Play.js
+++ Play.js
@@ -5,7 +5,8 @@ var pinky, meow;
 var haruki;
 var kagura;
 var speed = 10;
-var controller;
+var controller, pet = {x: 0};
+var bee, beeSpeed = 1;
 
 
 boil.Play.prototype = {
@@ -15,6 +16,9 @@ boil.Play.prototype = {
         game.load.spritesheet('meow', 'Assets/Spritesheets/meowMeow.png', 72, 69);
         game.load.spritesheet('haruki', 'Assets/Spritesheets/haruki.png', 190, 470);
         game.load.spritesheet('kagura', 'Assets/Spritesheets/kagura.png', 310, 500);
+        game.load.spritesheet('bee', 'Assets/Spritesheets/bee.png', 250, 200);
+        
+    
     },
     create: function(){
         game.physics.startSystem(Phaser.ARCADE);
@@ -25,6 +29,13 @@ boil.Play.prototype = {
         game.input.keyboard.addKey(Phaser.Keyboard.P).onDown.add(addPinky)
         game.input.keyboard.addKey(Phaser.Keyboard.M).onDown.add(addMeow)
         game.input.keyboard.addKey(Phaser.Keyboard.C).onDown.add(changeController)
+        game.input.keyboard.addKey(Phaser.Keyboard.J).onDown.add(changePet)
+        
+        bee=game.add.sprite(900, 500, 'bee');
+        bee.scale.setTo(0.22);
+        bee.animations.add('fly', [0,1,2,3]);
+        bee.animations.play('fly', 12, true);
+        
         
         haruki=game.add.sprite(130, 90, 'haruki');
         haruki.scale.setTo(0.20)
@@ -33,7 +44,7 @@ boil.Play.prototype = {
         kagura=game.add.sprite(50, 90, 'kagura');
         kagura.scale.setTo(0.19);
         kagura.animations.add('walk', [0,1,2,3,4,5,6]);
-        game.physics.enable([haruki, kagura])
+        game.physics.enable([haruki, kagura, bee])
         
         haruki.body.gravity.y=100
         kagura.body.gravity.y=100
@@ -44,7 +55,15 @@ boil.Play.prototype = {
         controller = haruki;
     },
     update: function(){
-        
+        game.physics.arcade.overlap(controller, bee, function(){console.log('hit');});
+        if(Math.abs(bee.x - controller.x) < 5) {
+            //nothing
+        }
+        else if(bee.x > controller.x) {
+            bee.x -= beeSpeed;
+        } else if(bee.x< controller.x) {
+            bee.x += beeSpeed;
+        }
         if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
             controller.x-=speed;
             controller.animations.play('walk', 12, true);
@@ -60,19 +79,42 @@ boil.Play.prototype = {
         if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
             controller.body.velocity.y = -60;
         }
+        
+        
+        if(pet.x === 0){
+            
+        }
+        else if (game.input.keyboard.isDown(Phaser.Keyboard.A)){
+            pet.x-=speed;
+            pet.animations.play('petWalk', 12, true);
+        }
+        else if(game.input.keyboard.isDown(Phaser.Keyboard.D)){
+         pet.x+=speed;
+            pet.animations.play('petWalk', 12, true);
+        }
     }
 }
          
 function addPinky(){
     pinky=game.add.sprite(100, 100, 'pinky');
-    pinky.animations.add('oreo', [0,1,2,3]);
-    pinky.animations.play('oreo', 4, true);
+    pinky.scale.setTo(0.7)
+    pinky.animations.add('petWalk', [0,1,2,3]);
+    pinky.animations.play('petWalk', 4, true);
+    game.physics.enable(pinky);
+    pinky.body.gravity.y=100
+    pinky.body.collideWorldBounds = true;
+    pet=pinky;
 }
 
 function addMeow(){
     meow=game.add.sprite(90, 200, 'meow');
-    meow.animations.add('milk', [0,1,2]);
-    meow.animations.play('milk', 4, true);
+    meow.scale.setTo(0.7)
+    meow.animations.add('petWalk', [0,1,2]);
+    meow.animations.play('petWalk', 4, true);
+    game.physics.enable(meow);
+    meow.body.gravity.y=100
+    meow.body.collideWorldBounds = true; 
+    pet=meow;
 }
 
 function changeController(){
@@ -82,6 +124,19 @@ function changeController(){
     }
     else{
         controller = haruki;
+    }
+} 
+
+function changePet(){
+    console.log('switching characters');
+    if(!meow || !pinky) {
+        return false;
+    }
+    if(pet == meow){
+        pet = pinky;
+    }
+    else{
+        pet = meow;
     }
 } 
 
